2f34085a6fec4c50b376e238b9c5b501ae6b5e32
Ghost General <awotundun@gmail.com>
2022-03-27 01:21:27 +0100

    romanNumeraltranskator function

--- index.js
+++ index.js
@@ -2,11 +2,37 @@ const os = require("os");
 const http = require("http");
 const url = require("url");
 
+
+
+const DIGIT_VALUES = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+};
+
+
 // Write a function that takes a roman numeral as input,
 // and returns the number as an integer
 // Don't remember roman numerals? Check https://www.mathsisfun.com/roman-numerals.html
 const romanNumeralTranslator = (str) => {
   // code goes here
+  let result = 0;
+  let splitedArr = str.split("");
+  for (let i = 0; i < splitedArr.length; i++) {
+    if (
+      i < splitedArr.length - 1 &&
+      DIGIT_VALUES[splitedArr[i]] < DIGIT_VALUES[splitedArr[i + 1]]
+    ) {
+      result -= DIGIT_VALUES[splitedArr[i]];
+    } else {
+      result += DIGIT_VALUES[splitedArr[i]];
+    }
+  }
+  return result;
 };
 
 const handleRequest = (req, res) => {
